3fa7faffe34717082a10ea418023f2351e678d58
jlarson2000 <jeff@circularlabs.com>
2024-02-06 03:12:18 +0000

    more notes

---
+++ UI/Source/examples/Labels.cpp
@@ -0,0 +1,15 @@
+
+// extracted from a Juce example
+#if 0
+    addAndMakeVisible (titleLabel);
+    titleLabel.setFont (juce::Font (16.0f, juce::Font::bold));
+    titleLabel.setText ("Click in the white box to enter some text...", juce::dontSendNotification);
+    titleLabel.setColour (juce::Label::textColourId, juce::Colours::lightgreen);
+    titleLabel.setJustificationType (juce::Justification::centred);
+
+    addAndMakeVisible (anotherLabel);
+    anotherLabel.setFont (juce::Font (16.0f, juce::Font::bold));
+    anotherLabel.setText ("More...", juce::dontSendNotification);
+    anotherLabel.setColour (juce::Label::textColourId, juce::Colours::lightgreen);
+    anotherLabel.setJustificationType (juce::Justification::centred);
+#endif
