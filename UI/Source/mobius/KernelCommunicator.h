02f5cc1be90b70f1a9775e41db79db796b768341
jlarson2000 <jeff@circularlabs.com>
2024-02-29 14:43:26 +0000

    start retooling communicator

--- UI/Source/mobius/KernelCommunicator.h
+++ UI/Source/mobius/KernelCommunicator.h
@@ -8,6 +8,42 @@
 #include <JuceHeader.h>
 
 /**
+ * KernelMessage type codes.
+ */
+typedefin enum {
+
+    MsgConfigure = 1,
+
+} MessageType;
+
+/**
+ * Objects that can be passed in a message.
+ * Union selection defined by the MessageType
+ */
+typedef union {
+
+    void* object;
+    class MobiusConfig* configuration;
+
+} MessageObject;
+
+/**
+ * Message object that can be passed up or down.
+ */
+class KernelMessage
+{
+  public:
+
+    // free pool chain
+    KernelMessage poolNext;
+
+    MessageType type;
+    MessageObject object;
+
+  private:
+};
+
+/**
  * Object used for passing information between the shell and the kernel.
  * Access to this will be guarded by a CriticalSection
  */
@@ -34,12 +70,36 @@ class KernelCommunicator
     // hmm, this is pretty verbose for this, easier just to block
     // on a single pendingConfiguration and returningConfiguration pointer?
 
+    // return the next shell or kernel message to process
+    // when finished just all free
+    KernelMessage* popShell();
+    KernelMessage* popKernel();
+
+    void pushShell(KernelMessage* msg);
+    void pushKernel(KernelMessage* msg);
+
+    KernelMessage* alloc();
+    void free(KernelMessage* msg);
+    
+    
   private:
 
+    juce::CriticalSection criticalSection;
+
+    // message pool
+    KernelMessage* pool = nullptr;
+    int poolSize = 0;
+
+    // shell message list
+    KernelMessage* toShell = nullptr;
+        
+    // kernel message list
+    KernelMessage* toKernel = nullptr;
+
+
+    // obsolete
     class MobiusConfig* sentConfiguration = nullptr;
     class MobiusConfig* returnedConfiguration = nullptr;
-
-    juce::CriticalSection criticalSection;
 
         
 };
